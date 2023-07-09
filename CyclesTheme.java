d1f2a60c4a56a52c6f965e6ec1082b12c2a73d78
Krutiverti <136634981+Krutiverti@users.noreply.github.com>
2023-07-09 20:53:42 +0300

    Add files via upload

--- CyclesTheme.java
+++ CyclesTheme.java
@@ -23,16 +23,15 @@ public class CyclesTheme {
         int num2 = -1;
         int num3 = 5;
         int min = num1;
-        if (num2 < min && num2 < num3) {
-            min = num2;
-        } else {
-            min = num3;
-        }
         int max = num2;
-        if (num1 > max && num1 > num3) {
+        if (num1 > num2) {
+            min = num2;
             max = num1;
-        } else {
+        }
+        if (max < num3) {
             max = num3;
+        } else if (num3 < min) {
+            min = num3;
         }
         for (int i = ++min; i < max; ++i) {
             System.out.print(i + " ");
@@ -40,16 +39,16 @@ public class CyclesTheme {
 
         //3. Вывод реверсивного числа и суммы его цифр
         System.out.println("\n\n3. Вывод реверсивного числа и суммы его цифр");
-        int baseDigit = 1234;
-        int sumDigit = 0;
+        int workNum= 1234;
+        int sumDigits = 0;
         System.out.print("Исходное число в обратном порядке: ");
-        while (baseDigit > 0) {
-            int reverseStep = baseDigit % 10;
-            sumDigit = sumDigit + (reverseStep);
-            System.out.print(reverseStep);
-            baseDigit /= 10;
+        while (workNum > 0) {
+            int remainder = workNum % 10;
+            sumDigits += remainder;
+            System.out.print(remainder);
+            workNum /= 10;
         }
-        System.out.println("\nСумма цифр в числе: " + sumDigit+ "\n");
+        System.out.println("\nСумма цифр в числе: " + sumDigits + "\n");
 
         //4. Вывод чисел на консоль в несколько строк
         System.out.println("4. Вывод чисел на консоль в несколько строк");
@@ -71,7 +70,7 @@ public class CyclesTheme {
 
         //5. Проверка количества двоек на четность/нечетность
         System.out.println("\n\n5. Проверка количества двоек на четность/нечетность");
-        int workNum = 3242592;
+        workNum = 3242592;
         System.out.print("Число " + workNum + " содержит ");
         int countTwos = 0;
         while (workNum > 0) {
@@ -97,89 +96,90 @@ public class CyclesTheme {
             System.out.println();
         }
         System.out.println();
-        int triangleSide = 5;
-        while (triangleSide > 0) {
-           int triangleSide2 = triangleSide;
-            while (triangleSide2 > 0) {
-                triangleSide2--;
+        int lang= 5;
+        while (lang > 0) {
+           int height = lang;
+            while (height > 0) {
+                height--;
                 System.out.print("#");
             }
             System.out.println();
-            triangleSide--;
+            lang--;
         }
 
-        int halfBase1 = 0;
+        lang = 0;
+        int countStrings = 0;
         do {
-            int cathetAB = 0;
+            if (countStrings < 3){
+                ++lang;
+            } else {
+                --lang;
+            }
             System.out.println();
-            halfBase1++;
+            int height = 0;
             do {
+                ++height;
                 System.out.print("$");
-                cathetAB++;
-            } while (cathetAB < halfBase1);
-        } while (halfBase1 < 3);
-        int halfBase2 = 2;
-        do {
-            int cathetBC = 2;
-            System.out.println();
-            halfBase2--;
-            do {
-                System.out.print("$");
-                cathetBC--;
-            } while (cathetBC == halfBase2);
-        } while (halfBase2 > 0);
+            }while (height < lang);
+            countStrings++;
+        }while (countStrings < 5);
+
+
 
         //7. Отображение ASCII-символов
         System.out.println("\n\n7. Отображение ASCII-символов");
         System.out.println("символы, идущие до цифр и имеющие нечетные коды");
-        char symbol = 0;
-        for (symbol = 33; symbol < 48; symbol++) {
-            if ( symbol % 2 != 0) {
-                System.out.printf("%3c\n", symbol);
+        for (int i = 33; i < 48; i++) {
+            if (i % 2 != 0) {
+                System.out.printf("%3d", i);
+                System.out.printf("%3c\n", i);
             }
         }
         System.out.println("маленькие английские буквы, имеющие четные коды");
-        int smallLetter = 0;
-        for (smallLetter = 97; smallLetter < 123; smallLetter++) {
-            if ( smallLetter % 2 == 0) {
-                System.out.printf("%3c\n", smallLetter);
+        for (int i = 97; i < 123; i++) {
+            if ( i % 2 == 0) {
+                System.out.printf("%3d", i);
+                System.out.printf("%3c\n", i);
             }
         }
 
         //8. Проверка, является ли число палиндромом
         System.out.println("\n8. Проверка, является ли число палиндромом");
-        int workDigit = 1234321;
-        int reverseDigit = 0;
-        for (int i = workDigit; i > 0 ; i /= 10) {
-            reverseDigit = reverseDigit * 10 + i % 10;
+        int palindrome = 1234321;
+        int reverse = 0;
+        for (int i = palindrome; i > 0; i /= 10) {
+            reverse = reverse * 10 + i % 10;
         }
-        if (reverseDigit == workDigit) {
-            System.out.println("число " + workDigit + " является палиндромом\n");
+        if (reverse == palindrome) {
+            System.out.println("число " + palindrome + " является палиндромом\n");
         } else {
-            System.out.println("число " + workDigit + " не является палиндромом\n");
+            System.out.println("число " + palindrome + " не является палиндромом\n");
         }
 
         //9. Определение, является ли число счастливым
         System.out.println("9. Определение, является ли число счастливым");
-        int happyValue = 452371;
-        int firstHalfValue = 0;
-        for (int i = happyValue % 1000; i > 0  ; i /= 10) {
-            firstHalfValue = firstHalfValue + i % 10;
+        int happyNum = 452371;
+        int rightHalf = happyNum % 1000;
+        int leftHalf = happyNum / 1000;
+        int sumRight = 0;
+        int sumLeft = 0;
+        for (int i = 0; i < 3; ++i) {
+            sumRight += rightHalf % 10;
+            rightHalf /= 10;
+            sumLeft += leftHalf % 10;
+            leftHalf /= 10;
         }
-        int secondHalfValue = 0;
-        for (int i = happyValue / 1000; i > 0  ; i /= 10) {
-            secondHalfValue = secondHalfValue + i % 10;
-        }
-        System.out.println("Сумма цифр " + happyValue % 1000 + " = " + firstHalfValue);
-        System.out.println("Сумма цифр " + happyValue / 1000 + " = " + secondHalfValue);
-        if (firstHalfValue == secondHalfValue) {
-            System.out.println("Число: " + happyValue + " Является счастливым\n");
+        System.out.println("Сумма цифр " + happyNum % 1000 + " = " + sumRight);
+        System.out.println("Сумма цифр " + happyNum / 1000 + " = " + sumLeft);
+        if (sumRight == sumLeft) {
+            System.out.println("Число: " + happyNum + " Является счастливым\n");
         } else {
-            System.out.println("Число: " + happyValue + " не является счастливым\n");
+            System.out.println("Число: " + happyNum + " не является счастливым\n");
         }
 
         //10. Вывод таблицы умножения Пифагора
         System.out.println("10. Вывод таблицы умножения Пифагора");
+        System.out.println("    Таблица пифагора");
         System.out.print(" |");
         for (int i = 2; i < 10 ; i++) {
             System.out.printf("%3d", i);
