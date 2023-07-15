5d1cd728f076fd06d6507fabc0a30faa7b94906c
Simon Bakumenko <semyonbakumenko@yandex.ru>
2023-07-15 14:05:49 +0300

    MyFirstGame

--- MyFirstGame.java
+++ MyFirstGame.java
@@ -1,1 +1,10 @@
+public class MyFirstGame {
 
+    public static void main (String[] args) {
+        int hideDigit = 56;
+        int playerDigit = 10;
+        if (playerDigit < hideDigit){
+            System.out.println();
+        }
+    }
+}
