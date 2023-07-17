3a0610d154d4d788cc9e671a9ba484d0f6713174
Simon Bakumenko <semyonbakumenko@yandex.ru>
2023-07-17 19:52:47 +0300

    MyFirstGameTestCommit

--- MyFirstGame.java
+++ MyFirstGame.java
@@ -1,10 +1,23 @@
 public class MyFirstGame {
 
     public static void main (String[] args) {
-        int hideDigit = 56;
-        int playerDigit = 10;
-        if (playerDigit < hideDigit){
-            System.out.println();
+        int hiddenDigit = 56;
+        int playerDigit = 99;
+        while (playerDigit != hiddenDigit) {
+            if (playerDigit < hiddenDigit) {
+                ++playerDigit;
+                System.out.println(playerDigit);
+            } else if (playerDigit > hiddenDigit) {
+                --playerDigit;
+                System.out.println(playerDigit);
+            }
         }
+        System.out.println( playerDigit + " win");
+
+
+
+
     }
-}
+
+    }
+
