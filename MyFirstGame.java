8ab532b0e06c407c816e5e7c3d607a5ca552948e
Simon Bakumenko <semyonbakumenko@yandex.ru>
2023-07-17 20:39:35 +0300

    MyFirstGameFinal

--- MyFirstGame.java
+++ MyFirstGame.java
@@ -1,23 +1,18 @@
 public class MyFirstGame {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         int hiddenDigit = 56;
-        int playerDigit = 99;
+        int playerDigit = 20;
         while (playerDigit != hiddenDigit) {
             if (playerDigit < hiddenDigit) {
-                ++playerDigit;
-                System.out.println(playerDigit);
+                System.out.println("Число " + playerDigit + ", меньше того, что загадал компьютер");
+                playerDigit += 5;
             } else if (playerDigit > hiddenDigit) {
+                System.out.println("Число " + playerDigit + ", больше того, что загадал компьютер");
                 --playerDigit;
-                System.out.println(playerDigit);
             }
         }
-        System.out.println( playerDigit + " win");
-
-
-
-
+        System.out.print("Вы победили");
     }
-
-    }
+}
 
